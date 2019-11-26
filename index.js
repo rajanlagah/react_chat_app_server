9ba77fc331e18261cc4f1d062e1b5c37b0a4a2a9
rajan <rajanlagah@gmail.com>
2019-11-26 15:18:04 +0530

    sending msg to clients

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const app = require('express')();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
-io.on('connection', () => { /* … */
+io.on('connection', (socket) => { /* … */
   console.log('conneted')
+  socket.on('message',(data)=>{
+    console.log('recived data',data);
+    // socket.emit('new-message', data) this will send only to client itself
+    io.emit('new-message', data) // this will send to all connection
+  })
 });
 server.listen(4000);
